fad32198ce4ee90c965de4f27d2a4f907f704b8b
julianmedwards <julian.michael.edwards@gmail.com>
2023-06-16 10:14:42 -0500

    Made env vars more explicit.

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: 'API',
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.API_PORT || 5000,
+    env: process.env.NODE_ENV,
+    port: process.env.API_PORT,
     db: {
-        name: process.env.DATABASE_NAME || 'trello-boards',
-        host: process.env.DATABASE_HOST || 'localhost',
-        username: process.env.DATABASE_USERNAME || 'root',
-        password: process.env.DATABASE_PASSWORD || 'password',
-        port: process.env.DATABASE_PORT || 3306,
+        name: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: process.env.DATABASE_PORT,
         dialect: process.env.DATABASE_DIALECT || 'mysql',
     },
 }
